19c2d30ad7cb0526af10c98a3adaad96ffd5acc3
Marc Coet <marcoet@gmail.com>
2016-12-05 13:38:16 +0100

    form is ok. Animations ok.

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
   var contactform =  document.getElementById('contactform');
   contactform.setAttribute('action', 'https://formspree.io/' + 'marcoet' + '@' + 'gmail' + '.' + 'com');
+
+
 })
