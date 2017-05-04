fbfd0b38b2c31553e1c607afa1d0a9ba1c30b850
MarcCoet <marcoet@gmail.com>
2017-05-04 16:53:12 +0200

    deleted everything

--- js/contact.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  var contactform =  document.getElementById('contactform');
-  contactform.setAttribute('action', 'https://formspree.io/' + 'marcoet' + '@' + 'gmail' + '.' + 'com');
-
-
-})
